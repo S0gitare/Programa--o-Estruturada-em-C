1ef8eef14ed160fcfd3d1b24a106fda836c673eb
S0gitare <alecsanderdavinci@gmail.com>
2025-07-29 18:55:32 -0300

    teste

---
+++ Exercicios Aula 0/exercicio_09.c
@@ -0,0 +1,12 @@
+/*Crie uma função chamada "parOuImpar" que recebe um número inteiro e imprime se ele é par ou ímpar. Use esta função em seu programa principal.*/
+
+#include <stdio.h>
+
+int main()
+{
+}
+
+int parOuImpar(int numero)
+{
+    
+}
