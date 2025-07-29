25e050d4ac31bf360d0a0b1b162db29cc2b1e72f
S0gitare <alecsanderdavinci@gmail.com>
2025-07-29 19:08:28 -0300

    teste

--- Exercicios Aula 0/exercicio_09.c
+++ Exercicios Aula 0/exercicio_09.c
@@ -4,24 +4,10 @@
 
 int main()
 {
-    int numero;
-
-    printf("Digite um numero inteiro: ");
-    scanf("%d", &numero);
-
-    parOuImpar(numero);
-
-    return 0;
+    
 }
 
 int parOuImpar(int numero)
 {
-    if (numero % 2 == 0)
-    {
-        printf("O numero %d é par.\n", numero);
-    }
-    else
-    {
-        printf("O numero %d é ímpar.\n", numero);
-    }
+    
 }
